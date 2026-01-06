3803979b1ce7b088fea5a8ad8060caa9ac75b620
vkim <vkim@tsc.uz>
2026-01-06 16:33:42 +0500

    added new function get gredentials

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from json import load
 from netbox_client import NetboxClient
-from utils import create_collector
+from utils import create_collector, get_credentials_by_platform
 import os
 from dotenv import load_dotenv
 
@@ -8,11 +9,7 @@ load_dotenv()
 NETBOX_TOKEN = os.getenv("NETBOX_TOKEN")
 NETBOX_URL = os.getenv("NETBOX_URL")
 
-credentials = {
-    "username": os.getenv("USERNAME"),
-    "password": os.getenv("PASSWORD"),
-    "port": 22,
-}
+
 
 
 nb = NetboxClient(NETBOX_URL, NETBOX_TOKEN)
@@ -21,7 +18,7 @@ devices = nb.get_devices(filters={"role": "sw"})
 
 for device in devices:
     ip = nb.get_device_ip(device)
-    
+    credentials = get_credentials_by_platform(device.platform.name)
     collector = create_collector(device.platform.name, ip, credentials)
 
     if hasattr(collector, 'connect'):
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 from huawei import HuaweiVRP, HuaweiYunShan
 from arista import AristaEOS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_collector(platform_name, host, credentials):
@@ -34,3 +38,27 @@ def create_collector(platform_name, host, credentials):
         )
     else:
         raise ValueError(f"Unknown platform: {platform_name}")
+
+
+def get_credentials_by_platform(platform_name):
+    platform_lower = platform_name.lower()
+
+    if "vrp" in platform_lower or "yunshan" in platform_lower:
+        return {
+            "username": os.getenv("HUAWEI_USERNAME"),
+            "password": os.getenv("HUAWEI_PASSWORD"),
+            "port": int(os.getenv("HUAWEI_PORT", "22")),
+        }
+
+    elif "eos" in platform_lower:
+        return {
+            "username": os.getenv("ARISTA_USERNAME"),
+            "password": os.getenv("ARISTA_PASSWORD"),
+            "port": int(os.getenv("ARISTA_PORT", "80")),
+        }
+    else:
+        return {
+            "username": os.getenv("USERNAME", "admin"),
+            "password": os.getenv("PASSWORD", ""),
+            "port": int(os.getenv("PORT", "22")),
+        }
